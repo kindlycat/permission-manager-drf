79cd583138188efd9119cb2d748f526aed3a3d6b
Grigory <kindlycat@users.noreply.github.com>
2024-05-18 07:26:20 +0400

    Add doctrings (#4)
    Co-authored-by: Grigory Mishchenko <grigory.mishchenko@gmail.com>

--- permission_manager_drf/settings.py
+++ permission_manager_drf/settings.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.utils.module_loading import import_string
 
 
+# Path to the function for getting a permission manager class for a model
 PERMISSION_MANAGER_DRF_FOR_MODEL_GETTER = getattr(
     settings,
     'PERMISSION_MANAGER_DRF_FOR_MODEL_GETTER',
     'permission_manager_drf.utils.get_permission_manager_class_for_model',
 )
 
+# Import the permission manager getter function dynamically
 permission_manager_getter = import_string(
     PERMISSION_MANAGER_DRF_FOR_MODEL_GETTER,
 )
